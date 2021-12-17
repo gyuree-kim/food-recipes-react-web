c8a80c4fcef8f6f12a5903353e2cb891a30f52ab
jongw <ljw8373@gmail.com>
2021-12-17 18:04:31 +0900

    Navigaion Bar 파일입니다
    fbase.js파일은 주말에 했던 상태로 해결이 안되어있고 recipeImage.js파일은
    firestore에서 파일을 불러와서 저장하는 파일로 이용하려고 생각하고 있어요

--- food-recipes/src/Components/Navbar/TopNavbar.js
+++ food-recipes/src/Components/Navbar/TopNavbar.js
@@ -4,15 +4,10 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import PostIcon from '@mui/icons-material/Article';
-import MoreIcon from '@mui/icons-material/MoreVert';
+import { useNavigate } from 'react-router';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -25,8 +20,9 @@ const Search = styled('div')(({ theme }) => ({
   marginLeft: 0,
   width: '100%',
   [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
+    left: '25%',
+    position: 'absolute',
+    width: '50%',
   },
 }));
 
@@ -34,6 +30,7 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 2),
   height: '100%',
   position: 'absolute',
+  right: '0',
   pointerEvents: 'none',
   display: 'flex',
   alignItems: 'center',
@@ -55,103 +52,29 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function PrimarySearchAppBar() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  let navigate = useNavigate();
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <PostIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+  function HomeClick() {
+    navigate("/");
+  }
+  function PostClick() {
+    navigate("/PostPage");
+  }
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+            onClick={HomeClick}
           >
             Food Recipes
-          </Typography>
+          </IconButton>
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
@@ -163,37 +86,16 @@ export default function PrimarySearchAppBar() {
           </Search>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+            <IconButton 
+              size="large" 
+              aria-label="show 4 new mails" 
+              color="inherit"
+              onClick={PostClick}>
               <PostIcon />
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
             </IconButton>
           </Box>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
     </Box>
   );
 }
--- food-recipes/src/fbase.js
+++ food-recipes/src/fbase.js
@@ -1,6 +1,8 @@
-import { initializeApp } from "firebase/app";
+/*import {initializeApp} from "firebase/app";
+import {getFirestore, doc, setDoc} from "firebase/firestore";
 
-const firebaseConfig = {
+
+const firebaseConfig = initializeApp({
   apiKey: "AIzaSyAuPk_BbZyZTYLwBQm_o-Xotz-KwDF5q0I",
   authDomain: "food-recipes-2b0f5.firebaseapp.com",
   databaseURL: "http://food-recipes-2b0f5.firebaseapp.com",
@@ -8,7 +10,20 @@ const firebaseConfig = {
   storageBucket: "food-recipes-2b0f5.appspot.com",
   messagingSenderId: "528137384189",
   appId: "1:528137384189:web:79528e08102cd791adce94",
-  measurementId: "G-3419JE7G00"
-};
+  measurementId: "G-3419JE7G00",
+});
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const firestore = getFirestore();
+
+const userDoc = doc(firestore, 'user/2');
+function writeUserId() {
+  const docData = {
+    userTest: 'v',
+  };
+  setDoc(userDoc, docData);
+}
+
+console.log('hello there, Firestore!')
+writeUserId();*/
