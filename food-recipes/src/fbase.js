8f224098ee030d28f545a7e21467ed2d9f9bf88d
jongw <ljw8373@gmail.com>
2021-12-18 19:16:07 +0900

    firebase 내용 추가
    firebase init후 추가 된 내용입니다.

--- food-recipes/src/fbase.js
+++ food-recipes/src/fbase.js
@@ -1,8 +1,8 @@
-/*import {initializeApp} from "firebase/app";
-import {getFirestore, doc, setDoc} from "firebase/firestore";
+import {initializeApp} from "firebase/app";
+import {getFirestore, collection, getDocs} from "firebase/firestore/lite";
 
 
-const firebaseConfig = initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyAuPk_BbZyZTYLwBQm_o-Xotz-KwDF5q0I",
   authDomain: "food-recipes-2b0f5.firebaseapp.com",
   databaseURL: "http://food-recipes-2b0f5.firebaseapp.com",
@@ -11,19 +11,14 @@ const firebaseConfig = initializeApp({
   messagingSenderId: "528137384189",
   appId: "1:528137384189:web:79528e08102cd791adce94",
   measurementId: "G-3419JE7G00",
-});
+};
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-const firestore = getFirestore();
-
-const userDoc = doc(firestore, 'user/2');
-function writeUserId() {
-  const docData = {
-    userTest: 'v',
-  };
-  setDoc(userDoc, docData);
+async function getUser(db) {
+  const userId = collection(db, 'users');
+  const userSnapshot = await getDocs(userId);
+  const userList = userSnapshot.docs.map(doc => doc.data());
+  return userList;
 }
-
-console.log('hello there, Firestore!')
-writeUserId();*/
