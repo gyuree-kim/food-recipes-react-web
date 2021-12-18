062d2fc2e95609a8296c889e802cca72f7e7906f
jongw <ljw8373@gmail.com>
2021-12-19 01:14:47 +0900

    modified navigation bar
    네비게이션 바 코드가 TopNavbar.js파일에 나와있습니다.

--- food-recipes/src/App.js
+++ food-recipes/src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
-import Home from './routers/Home';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PostPage from "./routers/PostPage";
-
 
 function App() {
   
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/PostPage" element={<PostPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>navigation</div>
   );
 }
 
