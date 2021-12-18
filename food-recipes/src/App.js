d5cfd029c67522292340298b8e47a51568a03ec1
gyuree-kim-dev <gyuree@peoplefund.co.kr>
2021-12-19 04:03:44 +0900

    firebase: usesrId 데이터 받아오기

--- food-recipes/src/App.js
+++ food-recipes/src/App.js
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from "react"
+import { getUser } from './firebase'
+
 function App() {
+  const [userId, setUserId] = useState("")
+
+  useEffect(() => {
+    async function getUserId() {
+      const userList = await getUser();
+      setUserId(userList[0].userId)
+    }
+    
+    getUserId();
+
+    return;
+  }, [setUserId])
+
   return (
     <div className="App">
-          Learn React
+          {userId}
     </div>
   );
 }
