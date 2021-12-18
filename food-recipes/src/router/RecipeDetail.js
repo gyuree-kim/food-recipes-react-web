6739b4ae0a956b4d847962a2136bda993828476b
SHParkhanyang <95166183+SHParkhanyang@users.noreply.github.com>
2021-12-19 07:55:36 +0900

    2
    2

--- food-recipes/src/router/RecipeDetail.js
+++ food-recipes/src/router/RecipeDetail.js
@@ -7,11 +7,32 @@ export default function FoodDetail() {
     // path에 " ":parameter " 로 선언되어있다면 useParams() hook으로 데려올 수 있습니다.
     // ex:) <Route path="/detail/:detailId ... />"
     let { detailId } = useParams();
+
+    const [postId, setPostId] = useState("")
+
+    useEffect(() => {
+      async function getPostId() {
+        const postList = await getPost();
+        //setPostId(postList[0].postId)
+
+        setPostId(postList)
+      }
+
+      getPostId();
+
+      return;
+    }, [setPostId])
+
+    var detailPost = postId.forEach(
+        function(x) {
+            return x.postId == detailId;
+        }
+    )
   
     return (
         <center>
             <StandardImageList></StandardImageList>
-            <div className="div_middle">{postId[0].postName}</div>
+            <div className="div_middle">{detailPost.postName}</div>
         </center>
     )
   }
